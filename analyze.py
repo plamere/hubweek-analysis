993cea11f9f6a5c6a75ca3c071041ec3c95087c0
Paul Lamere <paull@lon2-keanubackend-t1.lon2.spotify.net>
2015-10-08 23:30:59 +0000

    full run

--- analyze.py
+++ analyze.py
@@ -53,6 +53,10 @@ def perform_analysis(file):
             print 'trouble analyzing', file
             print 'skipping'
             print
+        except pyen.PyenException:
+            print 'trouble analyzing', file
+            print 'skipping'
+            print
         except requests.exceptions.ProxyError:
             print 'proxy trouble analyzing', file
             print 'skipping'
